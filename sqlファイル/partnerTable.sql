d90933509f6b37893312ac4937b4c0da81a44745
NemlessAlma <namelessalma@gmail.com>
2023-06-10 23:32:32 +0900

    PlutoProjectのSQL

---
+++ sqlファイル/partnerTable.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS `partner`;
+CREATE TABLE `partner` (
+  `apid` int(10) unsigned NOT NULL,
+  `pid` int(10) unsigned NOT NULL,
+  `lv` tinyint(3) unsigned NOT NULL,
+  `tlv` tinyint(3) unsigned NOT NULL,
+  `rb` tinyint(3) NOT NULL,
+  `rank` tinyint(3) unsigned NOT NULL,
+  `perkspoints` smallint(6) NOT NULL,
+  `perk0` tinyint(3) unsigned NOT NULL,
+  `perk1` tinyint(3) unsigned NOT NULL,
+  `perk2` tinyint(3) unsigned NOT NULL,
+  `perk3` tinyint(3) unsigned NOT NULL,
+  `perk4` tinyint(3) unsigned NOT NULL,
+  `perk5` tinyint(3) unsigned NOT NULL,
+  `aimode` tinyint(3) unsigned NOT NULL,
+  `basicai1` tinyint(3) unsigned NOT NULL,
+  `basicai2` tinyint(3) unsigned NOT NULL,
+  `hp` int(10) unsigned NOT NULL,
+  `maxhp` int(10) unsigned NOT NULL,
+  `mp` int(10) unsigned NOT NULL,
+  `maxmp` int(10) unsigned NOT NULL,
+  `sp` int(10) unsigned NOT NULL,
+  `maxsp` int(10) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
